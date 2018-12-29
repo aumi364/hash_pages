23b802fcc269568639e9da1009b8acf69d70ccb7
unknown <aumi.asif@gmail.com>
2018-12-30 02:03:34 +0600

    up to sldie login

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+  $("#loginButton").click(function(){
+      $(".login-section-slider").css({
+        "display":"block",
+        "visibility":"visible",
+        "height":"100vh",
+        "width":"100vw",
+        
+      });
+  });
+});
+
