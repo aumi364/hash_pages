15d4befd4a349cbbb3ad8d5132ab47ef87b303ad
aumi364 <aumi.asif@gmail.com>
2019-02-10 15:53:03 +0600

    reviewers profile completed

--- js/reviewers_profile.js
+++ js/reviewers_profile.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $(".modal-product-slider").owlCarousel({
     nav: true,
     autoplayHoverPause: true,
@@ -24,6 +24,21 @@ $(document).ready(function() {
       }
     }
   });
+});
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
+      $('#imagePreview').hide();
+      $('#imagePreview').fadeIn(650);
+    }
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+$("#imageUpload").change(function () {
+  readURL(this);
 });
 //  for minimizing the inner text of modal
 // $(document).ready(function () {
